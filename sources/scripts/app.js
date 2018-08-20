9e0f35dfd47298be320b828317fb67827d82ceab
Dziuba88 <Dziuba88@Gmail.com>
2018-08-20 20:13:31 +0300

    slider

--- sources/scripts/app.js
+++ sources/scripts/app.js
@@ -1,7 +1,12 @@
 $('[data-carousel=viewed]').owlCarousel({
   autoWidth:true,
-  items : 1,
   dots: false,
   navs: false,
-  responsiveRefreshRate: 0
-});
+  responsiveRefreshRate: 0,
+  responsive : {
+    0 : {items : 1},
+    769 : {items : 2},
+    961 : {items : 3},
+    1241 : {items : 4},
+  }
+})
